33c87b511624e9604380fccb6b4dc47bbeac3b04
DANTES0 <kzstar18rus@gmail.com>
2023-08-21 19:30:52 +0400

    Бургер меню, не на ту кнопку

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,14 +1,33 @@
+import { useState } from "react"
+import Menu from "../Menu/Menu"
 import NavBarLeftPanel from "../NavBarLeftPanel/NavBarLeftPanel"
 import "./NavBar.css"
 
 function NavBar() {
+  const [menuActive, setMenuActive] = useState(false)
+  const items = [
+    { value: "Главная", href: "/main", icon: "title" },
+    { value: "Профиль", href: "/profile", icon: "title" },
+    { value: "Галлерея", href: "/gallery", icon: "title" },
+  ]
   return (
     <div className="navmenu">
       <NavBarLeftPanel />
       <div className="navmenu__right__panel">
-        <button className="navmenu__btn">+</button>
+        <button
+          className="navmenu__btn"
+          onClick={() => setMenuActive(!menuActive)}
+        >
+          +
+        </button>
         <input type="text" className="navmenu__search" placeholder="Поиск..." />
       </div>
+      <Menu
+        active={menuActive}
+        setActive={setMenuActive}
+        header={"Бургер меню"}
+        items={items}
+      />
     </div>
   )
 }
